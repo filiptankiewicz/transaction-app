f8bc76a74df6cb09875a2cf97031e6d28cba7b5a
Filip <filiptankiewicz04@gmail.com>
2023-01-19 22:59:52 +0100

    single page application added

--- src/helpers/navigation/index.js
+++ src/helpers/navigation/index.js
@@ -19,24 +19,61 @@ const ROUTES = {
   PROFILE: "profile"
 };
 
-function linkTo(route){
-  window.location.href = `#/${route}`;
+let page = null;
+
+const renderSignIn = () => {
+ 
+  document.getElementById("root").innerHTML = homeHTML+loginHTML;
+  document.getElementById(SELECTORS.LOGIN_BUTTON).onclick = () => signIn(); 
+  page = ROUTES.SIGNIN;
 }
 
+const renderSignUp = () => {
+  
+  document.getElementById("root").innerHTML = homeHTML+registrationHTML; 
+  document.getElementById(SELECTORS.REGISTER_BUTTON).onclick = () => signUp();
+  page = ROUTES.SIGNUP;
+}
 
+const renderProfile = () => {
 
-function  goToUserProfilePage(){
-  linkTo(ROUTES.PROFILE);
   document.getElementById(SELECTORS.ROOT).innerHTML = profileHTML; 
-
   callToAPI();
   createBarChart();
   createPieChart();
   document.getElementById(SELECTORS.SIGN_OUT).onclick = () => signOut();
   document.getElementById(SELECTORS.USER_NAME).innerHTML = `Hej ${localStorage.getItem("userData_Name")}`;
+  page = ROUTES.PROFILE;
 
+}
 
+const currentFullUrlLocation = window.location.href;
+ const currentPage = currentFullUrlLocation.split('#/')[1]
 
+ document.getElementById("root").innerHTML = homeHTML;
+
+ switch(currentPage)
+ {
+    case ROUTES.SIGNIN:
+      renderSignIn();
+    break;
+    
+    case ROUTES.SIGNUP:
+      renderSignUp();
+    break;
+
+    case ROUTES.PROFILE:
+      renderProfile();
+    break;
+ }
+
+function linkTo(route){
+  window.location.href = `#/${route}`;
+  window.location.reload();
+}
+
+function  goToUserProfilePage(){
+  linkTo(ROUTES.PROFILE);
 };
 
 function goToHomePage() {
--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
 (function() {
-    
-    document.getElementById("root").innerHTML = homeHTML;
+    if(page !== ROUTES.PROFILE)
+    {
+        document.getElementById(SELECTORS.SIGN_IN_BUTTON).onclick = () => { 
+            linkTo(ROUTES.SIGNIN);  
+        };
 
-    document.getElementById(SELECTORS.SIGN_IN_BUTTON).onclick = () => { 
-        linkTo(ROUTES.SIGNIN);  
-        document.getElementById("root").innerHTML = homeHTML+loginHTML;
-
-        document.getElementById(SELECTORS.LOGIN_BUTTON).onclick = () => signIn();
-     };
-
-    document.getElementById(SELECTORS.SIGN_UP_BUTTON).onclick = () => { 
-        linkTo(ROUTES.SIGNUP);
-        document.getElementById("root").innerHTML = homeHTML+registrationHTML; 
-        document.getElementById(SELECTORS.REGISTER_BUTTON).onclick = () => signUp();
-    };
+        document.getElementById(SELECTORS.SIGN_UP_BUTTON).onclick = () => { 
+            linkTo(ROUTES.SIGNUP);
+        };
+    }
 
 })()
    
